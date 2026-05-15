5ea9e967cbf6c01bc16a9c4b439de479c72da72c
konstantine <kende23@freeuni.edu.ge>
2026-05-15 23:10:01 +0400

    dataclasses finished

---
+++ src/language/entities.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class AxesEntry:
+    agency: int = 0
+    physicality: int = 0
+    social: int = 0
+    system: int = 0
+
+@dataclass
+class TagEntry:
+    tag: list[str]
+
+@dataclass
+class NounEntry:
+    word: str
+    tag: TagEntry
+    axes: AxesEntry
+
+@dataclass
+class VerbToNounConstraintEntry:
+    tag: TagEntry
+    axes_min: AxesEntry
+    axes_max: AxesEntry
+
+@dataclass
+class VerbArgumentEntry:
+    verb_to_subject_constraint: VerbToNounConstraintEntry
+    verb_to_object_constraint: Optional[VerbToNounConstraintEntry]
+
+@dataclass
+class VerbEntry:
+    tag: TagEntry
+    axes: AxesEntry
+    verb_argument: VerbArgumentEntry
+
+@dataclass
+class AdjectiveToNounConstraint:
+    tag: TagEntry
+    axes_min: AxesEntry
+    axes_max: AxesEntry
+
